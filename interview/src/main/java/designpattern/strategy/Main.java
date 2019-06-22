9f49c303fd09b840d4b06d9b9c2726a900fb45e7
sunyk <sunyukun@shopin.cn>
2019-06-22 15:48:48 +0800

    design pattern,strategy pattern refactor

--- interview/src/main/java/designpattern/strategy/Main.java
+++ interview/src/main/java/designpattern/strategy/Main.java
@@ -4,17 +4,19 @@ package designpattern.strategy;
  * @author: sun.yukun@foxmail.com
  * Time: 2019-06-22 14:14
  * Blog: coderdaily.net
+ * <p>
+ * https://www.runoob.com/design-pattern/strategy-pattern.html
  */
 public class Main {
     public static void main(String[] args) {
         //创建一个 OperationAdd 对象，传给 Context，我们并没有去执行 OperationAdd.doOperation 方法
         OperationAdd add = new OperationAdd();
         Context context = new Context(add);
-        System.out.println(context.executeStrategy(2, 3));
+        System.out.println(context.executeStrategy(2, 3));//5
 
         //同理
         OperationMultiply multiply = new OperationMultiply();
         context = new Context(multiply);
-        System.out.println(context.executeStrategy(2, 3));
+        System.out.println(context.executeStrategy(2, 3));//6
     }
 }
