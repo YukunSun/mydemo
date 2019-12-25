49349b8fd9c7cd0ab1a4896d8030704f6418b9c5
sunyk <sunyukun@shopin.cn>
2019-12-25 09:01:01 +0800

    generic:method

--- jdkdemo/src/test/java/net/coderdaily/generics/ExampleTest3.java
+++ jdkdemo/src/test/java/net/coderdaily/generics/ExampleTest3.java
@@ -2,8 +2,8 @@ package net.coderdaily.generics;
 
 import org.junit.Test;
 
-import java.util.EnumSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author: sun.yukun@foxmail.com
@@ -21,6 +21,47 @@ public class ExampleTest3 {
         System.out.println("Sets.difference(s1,s2) = " + Sets.difference(s1, s2));
         System.out.println("Sets.union(s1,s2) = " + Sets.union(s1, s2));
         System.out.println("Sets.complement(s1,s2) = " + Sets.complement(s1, s2));
+    }
+
+    @Test
+    public void methodDifference() {
+        ContainerMethodDifferences cmd = new ContainerMethodDifferences();
+        System.out.println("cmd.methodSet(Collection.class) = " + cmd.methodSet(Collection.class));
+        System.out.println("cmd.interfaces(Collection.class) = " + cmd.interfaces(Collection.class));
+
+        System.out.println("cmd.object = " + cmd.object);
+
+        cmd.difference(Set.class, Collection.class);
+        cmd.difference(HashSet.class, Set.class);
+    }
+}
+
+
+class ContainerMethodDifferences {
+    public static Set<String> methodSet(Class<?> type) {
+        Set<String> result = new HashSet<>();
+        Arrays.stream(type.getMethods()).map(m -> result.add(m.getName())).collect(Collectors.toSet());
+        return result;
+    }
+
+    public static Set<String> interfaces(Class<?> type) {
+        Set<String> result = new HashSet<>();
+        Arrays.stream(type.getInterfaces()).map(m -> result.add(m.getSimpleName())).collect(Collectors.toSet());
+        return result;
+    }
+
+    static Set<String> object = methodSet(Object.class);
+
+    static {
+        //native method
+        object.add("clone");
+    }
+
+    static void difference(Class<?> superset, Class<?> subset) {
+        Set<String> diff = Sets.difference(methodSet(superset), methodSet(subset));
+        diff.removeAll(object);
+        System.out.println("diff = " + diff);
+        System.out.println("interfaces(superset) = " + interfaces(superset));
     }
 }
 
