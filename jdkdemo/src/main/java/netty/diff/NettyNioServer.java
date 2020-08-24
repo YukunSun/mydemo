b076c33e189ad1f18c23c1b1ac2b11af5b26b085
sunyk <sunyukun@flipboard.cn>
2020-08-24 15:35:02 +0800

    netty nio

--- jdkdemo/src/main/java/netty/diff/NettyNioServer.java
+++ jdkdemo/src/main/java/netty/diff/NettyNioServer.java
@@ -4,6 +4,8 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.oio.OioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
@@ -21,13 +23,18 @@ public class NettyNioServer {
         ByteBuf buf = Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("NettyNioServer send data...", Charset.forName("UTF-8")));
         //1 同步的API已经被废弃
 //        EventLoopGroup group = new OioEventLoopGroup();
+        //2 基于 jdk nio 的实现方式
         EventLoopGroup group = new NioEventLoopGroup();
+        //3 基于 Linux epoll 的实现方式
+//        EventLoopGroup group = new EpollEventLoopGroup();
         try {
             ServerBootstrap bootstrap = new ServerBootstrap();
             bootstrap.group(group)
-                    //2 同步的API已被废弃
+                    //1 同步的API已被废弃
 //                    .channel(OioServerSocketChannel.class)
+                    //2
                     .channel(NioServerSocketChannel.class)
+//                    .channel(channelEpollServerSocketChannel.class)
                     .localAddress(new InetSocketAddress(port))
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
