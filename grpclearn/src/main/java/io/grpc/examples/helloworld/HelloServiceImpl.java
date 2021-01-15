37b231e635061cae2f58c8953d1c27d952bd6cb8
sunyk <sunyukun@flipboard.cn>
2021-01-15 17:31:04 +0800

    newFutureStub

--- grpclearn/src/main/java/io/grpc/examples/helloworld/HelloServiceImpl.java
+++ grpclearn/src/main/java/io/grpc/examples/helloworld/HelloServiceImpl.java
@@ -2,6 +2,8 @@ package io.grpc.examples.helloworld;
 
 import io.grpc.stub.StreamObserver;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * @author sunyk
  **/
@@ -10,8 +12,43 @@ public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
         System.out.println("req msg:" + request.toString());
 
-        HelloReply reply = HelloReply.newBuilder().setMsg("Trump over...").build();
+        randomSleep();
+
+        HelloReply reply = HelloReply.newBuilder().setMsg("response...age:" + request.getAge()).build();
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void sayHelloSSRpc(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        System.out.println("req msg:" + request.toString());
+        HelloReply reply = HelloReply.newBuilder().setMsg("response...age:" + request.getAge()).build();
+
+        randomSleep();
+
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public StreamObserver<HelloRequest> sayHelloCSRpc(StreamObserver<HelloReply> responseObserver) {
+
+
+        return super.sayHelloCSRpc(responseObserver);
+    }
+
+    @Override
+    public StreamObserver<HelloRequest> sayHelloBidRpc(StreamObserver<HelloReply> responseObserver) {
+
+
+        return super.sayHelloBidRpc(responseObserver);
+    }
+
+    void randomSleep() {
+        try {
+            Thread.sleep(1000 * ThreadLocalRandom.current().nextInt(10));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- grpclearn/src/test/java/io/grpc/examples/hello/HelloWorldTest.java
+++ grpclearn/src/test/java/io/grpc/examples/hello/HelloWorldTest.java
@@ -1,6 +1,7 @@
 package io.grpc.examples.hello;
 
 import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.ListenableFuture;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.Server;
@@ -12,7 +13,9 @@ import io.grpc.examples.helloworld.HelloServiceImpl;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author sunyk
@@ -25,7 +28,8 @@ public class HelloWorldTest {
                 .build();
 
         server.start();
-        server.awaitTermination(1, TimeUnit.MINUTES);
+//        server.awaitTermination(5, TimeUnit.MINUTES);
+        server.awaitTermination();
     }
 
     @Test
@@ -36,10 +40,43 @@ public class HelloWorldTest {
                 .build();
         HelloServiceGrpc.HelloServiceBlockingStub blockingStub = HelloServiceGrpc.newBlockingStub(channel);
 
-        HelloRequest request = HelloRequest.newBuilder().setAge(13).setName("foo").setSex(false).addAllFav(Lists.newArrayList("Math", "English")).build();
-        HelloReply response = blockingStub.sayHello(request);
-        System.out.println("response:" + response);
+        for (int i = 0; i < 10; i++) {
+            HelloRequest request = HelloRequest.newBuilder().setAge(i).setName("foo").setSex(false).addAllFav(Lists.newArrayList("Math", "English")).build();
+            HelloReply response = blockingStub.sayHello(request);
+            System.out.println("response:" + response);
+        }
 
         channel.shutdown();
+    }
+
+    @Test
+    public void helloClient2() {
+        String target = "localhost:50051";
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(target)
+                .usePlaintext()
+                .build();
+        HelloServiceGrpc.HelloServiceFutureStub futureStub = HelloServiceGrpc.newFutureStub(channel);
+
+        for (int i = 0; i < 10; i++) {
+            HelloRequest request = HelloRequest.newBuilder().setAge(i).setName("foo").setSex(false).addAllFav(Lists.newArrayList("Math", "English")).build();
+            ListenableFuture<HelloReply> reply = futureStub.sayHello(request);
+
+            try {
+                System.out.println("response:" + reply.get(2, TimeUnit.SECONDS));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            }
+        }
+
+        channel.shutdown();
+    }
+
+    @Test
+    public void helloStreamClient() {
+
     }
 }
