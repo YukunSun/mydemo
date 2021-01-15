820f8bfeed29317349687bd358c29438399962bc
sunyk <sunyukun@flipboard.cn>
2021-01-15 14:32:13 +0800

    proto grpc generate

--- grpclearn/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
+++ grpclearn/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
@@ -1,10 +1,15 @@
 package io.grpc.examples.helloworld;
 
+import io.grpc.ManagedChannelBuilder;
+
 /**
  * @author sunyk
  **/
 public class HelloWorldClient {
-    public static void main(String[] args) {
+//    GreeterGrpc.GreeterBlockingStub blockingStub =
 
+    public static void main(String[] args) {
+        String target = "localhost:50051";
+        ManagedChannelBuilder<?> channel = ManagedChannelBuilder.forTarget(target);
     }
 }
