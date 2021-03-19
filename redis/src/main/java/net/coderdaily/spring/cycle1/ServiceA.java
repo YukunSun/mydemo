87090f24e8dc78e2e9537f088308b3fc369fbb72
sunyk <sunyukun@shopin.cn>
2021-03-19 19:03:51 +0800

    comment

--- redis/src/main/java/net/coderdaily/spring/cycle1/ServiceA.java
+++ redis/src/main/java/net/coderdaily/spring/cycle1/ServiceA.java
@@ -1,5 +1,6 @@
 package net.coderdaily.spring.cycle1;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 /**
@@ -7,9 +8,10 @@ import org.springframework.stereotype.Component;
  * Time: 2021/3/10 19:31
  * Blog: bengle.me
  * <p>
- * 构造函数创建，解决不了这个循环依赖，就像一个鸡生蛋的问题
+ * 构造函数创建，解决不了这个循环依赖，就像一个鸡生蛋的问题；
+ * 这里显式指定了要用构造器的方法来构造bean
  */
-@Component
+@Component(value = "c1serviceA")
 public class ServiceA {
     private ServiceB serviceB;
 
--- redis/src/main/java/net/coderdaily/spring/cycle1/ServiceB.java
+++ redis/src/main/java/net/coderdaily/spring/cycle1/ServiceB.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
  * Time: 2021/3/10 19:32
  * Blog: bengle.me
  */
-@Component
+@Component(value = "c1serviceB")
 public class ServiceB {
     private ServiceA serviceA;
 
