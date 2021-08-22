2bb4f7b5dc5500d229eeb18fb3eab78415867578
yukunsun <yukunsun@didiglobal.com>
2021-08-22 18:14:09 +0800

    cloud：communicate with resttemplate

--- spring-cloud-demo/cloud-consumer-dept/src/main/java/com/sunyukun/cloudconsumerdept/CloudConsumerDeptApplication.java
+++ spring-cloud-demo/cloud-consumer-dept/src/main/java/com/sunyukun/cloudconsumerdept/CloudConsumerDeptApplication.java
@@ -1,9 +1,11 @@
 package com.sunyukun.cloudconsumerdept;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @SpringBootApplication
@@ -13,8 +15,16 @@ public class CloudConsumerDeptApplication {
         SpringApplication.run(CloudConsumerDeptApplication.class, args);
     }
 
+    private static final RestTemplate restTemplate = new RestTemplate();
+    public static final String PROVIDER = "http://localhost:8001/provider/";
+
     @RequestMapping(value = "ping")
     public String ping() {
         return "pong";
+    }
+
+    @RequestMapping(value = "dept")
+    public String getDept() {
+        return restTemplate.getForObject(PROVIDER + "dept/list", String.class);
     }
 }
