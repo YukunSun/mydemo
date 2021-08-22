491b7ab9d78df57e9ab6758fe1858c12ce3e58d8
yukunsun <yukunsun@didiglobal.com>
2021-08-22 21:44:54 +0800

    cloud：eureka init

--- spring-cloud-demo/cloud-consumer-dept/src/main/java/com/sunyukun/cloudconsumerdept/CloudConsumerDeptApplication.java
+++ spring-cloud-demo/cloud-consumer-dept/src/main/java/com/sunyukun/cloudconsumerdept/CloudConsumerDeptApplication.java
@@ -1,8 +1,8 @@
 package com.sunyukun.cloudconsumerdept;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -23,7 +23,7 @@ public class CloudConsumerDeptApplication {
         return "pong";
     }
 
-    @RequestMapping(value = "dept")
+    @GetMapping(value = "dept")
     public String getDept() {
         return restTemplate.getForObject(PROVIDER + "dept/list", String.class);
     }
