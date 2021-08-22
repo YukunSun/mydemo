f2fb72a5347cf21489733e289f70676adc9b4dd2
yukunsun <yukunsun@didiglobal.com>
2021-08-22 22:57:37 +0800

    cloud：eureka integrated

--- spring-cloud-demo/cloud-consumer-dept/src/main/java/com/sunyukun/cloudconsumerdept/CloudConsumerDeptApplication.java
+++ spring-cloud-demo/cloud-consumer-dept/src/main/java/com/sunyukun/cloudconsumerdept/CloudConsumerDeptApplication.java
@@ -2,6 +2,7 @@ package com.sunyukun.cloudconsumerdept;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @SpringBootApplication
+@EnableEurekaClient
 public class CloudConsumerDeptApplication {
 
     public static void main(String[] args) {
--- spring-cloud-demo/cloud-provide-dept/src/main/java/com/sunyukun/cloudprovidedept/CloudProvideDeptApplication.java
+++ spring-cloud-demo/cloud-provide-dept/src/main/java/com/sunyukun/cloudprovidedept/CloudProvideDeptApplication.java
@@ -2,11 +2,13 @@ package com.sunyukun.cloudprovidedept;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@EnableEurekaClient
 public class CloudProvideDeptApplication {
 
     public static void main(String[] args) {
