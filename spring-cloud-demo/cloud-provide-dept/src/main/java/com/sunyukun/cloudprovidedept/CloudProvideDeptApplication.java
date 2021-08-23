d206f383f8cbc314282e562c4e8b71a4eea2162f
yukunsun <yukunsun@didiglobal.com>
2021-08-23 09:17:11 +0800

    cloud：eureka discovery

--- spring-cloud-demo/cloud-provide-dept/src/main/java/com/sunyukun/cloudprovidedept/CloudProvideDeptApplication.java
+++ spring-cloud-demo/cloud-provide-dept/src/main/java/com/sunyukun/cloudprovidedept/CloudProvideDeptApplication.java
@@ -1,10 +1,16 @@
 package com.sunyukun.cloudprovidedept;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @SpringBootApplication
@@ -18,5 +24,20 @@ public class CloudProvideDeptApplication {
     @RequestMapping(value = "ping")
     public String ping() {
         return "pong";
+    }
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping(value = "dept/discovery")
+    public String deptDiscovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String svc : services) {
+            List<ServiceInstance> instances = discoveryClient.getInstances(svc);
+            for (ServiceInstance ins : instances) {
+                System.out.println("ins = " + ins);
+            }
+        }
+        return String.join(",", services);
     }
 }
