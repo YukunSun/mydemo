1d398f508dc55be942aa6349dcbe908a824c7da3
sun.yukun <sun.yukun@foxmail.com>
2024-06-03 09:40:51 +0800

    aviator

--- aviator/aviator/src/test/java/com/sunyukun/AviatorTest.java
+++ aviator/aviator/src/test/java/com/sunyukun/AviatorTest.java
@@ -25,4 +25,23 @@ public class AviatorTest extends TestCase {
             exp.execute();
         }
     }
+
+    public void testMap() {
+        String expression = "a-(b-c) > 100";
+        Expression compiledExp = AviatorEvaluator.compile(expression);
+        // Execute with injected variables.
+        Boolean result = (Boolean) compiledExp.execute(compiledExp.newEnv("a", 100.3, "b", 45, "c", -199.100));
+        System.out.println(result);
+    }
+
+    public void testValidate() {
+        AviatorEvaluator.validate("1 +* 2");
+    }
+
+
+    
+    public void testName() {
+        //todo 引擎模式：https://www.yuque.com/boyan-avfmj/aviatorscript/fycwgt
+        //todo 最佳实践：https://www.yuque.com/boyan-avfmj/aviatorscript/ou23gy
+    }
 }
